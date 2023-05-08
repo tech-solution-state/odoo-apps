9cdb794f50d51c7ee3bc17d1dcdc6c166133333d
tech-solution-state <techsolutionstate@gmail.com>
2023-05-08 17:30:06 +0600

    description modified

--- numbering_list_tss/__manifest__.py
+++ numbering_list_tss/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 {
-    'name': "Serial Number on List View",
-    'summary': 'Displaying Serial Number on List View.',
-    'description': """By installing this module, user can see serial number in every list view. numbering list, sequential number in list, row number in list, numberring tree, sequential number in tree, row number in tree, auto numbering in tree/list""",
+    'name': "Serial Number in List View",
+    'summary': 'Displaying Serial Number in List View.',
+    'description': """By installing this module, user can see serial number in list view. numbering list, sequential number in list, row number in list, numberring tree, sequential number in tree, row number in tree, auto numbering in tree/list""",
     'version': '14.0.0.1',
     'category': 'Other',
     'license': 'LGPL-3',
